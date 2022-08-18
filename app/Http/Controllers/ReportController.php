b15919ccdda1a652b5d396fb12702e72e8df0101
Wilfried DJOPA <wilfried.djopa@gmail.com>
2022-08-18 19:18:54 +0000

    feat: implemented pagination on reports

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -26,9 +26,7 @@ class ReportController extends Controller
         if($status != null)
             $reports = $reports->where('status', $status);
 
-        $reports = ReportResource::collection($reports);
-
-        return response()->json($reports->simplePaginate($perPage));
+        return response()->json(ReportResource::collection($reports->simplePaginate($perPage)));
     }
 
     /**
