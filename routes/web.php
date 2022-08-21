1305b8d3f4d09e6bd7977c75403fc1de02a2933f
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2022-08-21 04:57:28 +0100

    add redirect from root to admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function() {
+    return redirect('/admin');
+});
+
 Route::prefix('admin')->group(function () {
     Voyager::routes();
 });
